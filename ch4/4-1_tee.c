9650c113e468897fa8adda3915ccdcc2ea5f24f1
Andrew Gilbert <andrewgilbert12@gmail.com>
2017-11-19 16:14:47 +0900

    ch 4 format fix

--- ch4/4-1_tee.c
+++ ch4/4-1_tee.c
@@ -7,10 +7,9 @@
 
 // Simple implementation of tee
 
-int
-main (int argc, char ** argv){
+int main (int argc, char ** argv) {
 
-  if(argc < 2 || argc > 3){
+  if(argc < 2 || argc > 3) {
     printf("%s [-a] file", argv[0]);
     exit(EXIT_FAILURE);
   }
@@ -18,7 +17,7 @@ main (int argc, char ** argv){
   int append, opt;
 
   append = 0;
-  while((opt = getopt(argc, argv, "a")) != -1){
+  while((opt = getopt(argc, argv, "a")) != -1) {
     switch(opt) {
     case 'a':
       append = 1;
@@ -34,7 +33,7 @@ main (int argc, char ** argv){
   file = open(argv[optind],
               O_WRONLY | O_CREAT | (append ? O_APPEND : O_TRUNC),
               S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-  if(file == -1){
+  if(file == -1) {
     perror("Error opening file");
     exit(EXIT_FAILURE);
   }
@@ -46,26 +45,26 @@ main (int argc, char ** argv){
   char buffer[BUFFER_SIZE];
 
   ssize_t readsize, writesize;
-  while((readsize = read(STDIN_FILENO, buffer, BUFFER_SIZE))){
+  while((readsize = read(STDIN_FILENO, buffer, BUFFER_SIZE))) {
     // write to stdout
-    if((writesize = write(STDOUT_FILENO, buffer, readsize)) == -1){
+    if((writesize = write(STDOUT_FILENO, buffer, readsize)) == -1) {
       perror("Write to STDOUT error");
       exit(EXIT_FAILURE);
     }
 
     // write to file 
-    if((writesize = write(file, buffer, readsize)) == -1){
+    if((writesize = write(file, buffer, readsize)) == -1) {
       perror("Write to file error");
       exit(EXIT_FAILURE);
     }
   }
 
-  if(readsize == -1){
+  if(readsize == -1) {
     perror("Read error");
     exit(EXIT_FAILURE); 
   }
 
-  if(close(file) == -1){
+  if(close(file) == -1) {
     perror("Close file error");
     exit(EXIT_FAILURE);
   }
--- ch4/4-2_cp.c
+++ ch4/4-2_cp.c
@@ -12,9 +12,8 @@ void print_help(char *filename)
   printf("%s src dest\n", filename);
 }
 
-int main (int argc, char ** argv)
-{
-  if(argc != 3){ // TODO change after adding options
+int main (int argc, char ** argv) {
+  if(argc != 3) { // TODO change after adding options
     print_help(argv[0]);
     exit(EXIT_FAILURE);
   }
@@ -22,7 +21,7 @@ int main (int argc, char ** argv)
   int opt;
 
   /* no options */
-  while((opt = getopt(argc, argv, "a")) != -1){
+  while((opt = getopt(argc, argv, "a")) != -1) {
     switch(opt) {
     default:
       print_help(argv[0]);
@@ -32,18 +31,18 @@ int main (int argc, char ** argv)
 
   int src, dest;
 
-  if((src = open(argv[optind], O_RDONLY)) == -1){
+  if((src = open(argv[optind], O_RDONLY)) == -1) {
     printf("Couldn't open %s\n", argv[optind]);
     exit(EXIT_FAILURE);
   }
 
   struct stat *srcstats;
-  if(fstat(src, srcstats) == -1){
+  if(fstat(src, srcstats) == -1) {
     perror("Couldn't stat source file.");
     exit(EXIT_FAILURE);
   }
 
-  if((dest = open(argv[optind + 1], O_WRONLY | O_CREAT | O_TRUNC, srcstats->st_mode)) == -1){ // TODO won't reset permissions for an already existing file?
+  if((dest = open(argv[optind + 1], O_WRONLY | O_CREAT | O_TRUNC, srcstats->st_mode)) == -1) { // TODO won't reset permissions for an already existing file?
     printf("Couldn't open %s\n", argv[optind]);
     exit(EXIT_FAILURE);
   }
@@ -55,25 +54,25 @@ int main (int argc, char ** argv)
   char buffer[BUFFER_SIZE];
 
   ssize_t readsize, writesize;
-  while((readsize = read(src, buffer, BUFFER_SIZE))){
+  while((readsize = read(src, buffer, BUFFER_SIZE))) {
     // write to stdout
-    if((writesize = write(dest, buffer, readsize)) == -1){
+    if((writesize = write(dest, buffer, readsize)) == -1) {
       perror("Copy to dest error"); 
       exit(EXIT_FAILURE);
     }
   }
 
-  if(readsize == -1){
+  if(readsize == -1) {
     perror("Read error");
     exit(EXIT_FAILURE); 
   }
 
-  if(close(src) == -1){
+  if(close(src) == -1) {
     perror("Close src error");
     exit(EXIT_FAILURE);
   }
 
-  if(close(dest) == -1){
+  if(close(dest) == -1) {
     perror("Close dest error");
     exit(EXIT_FAILURE);
   }
